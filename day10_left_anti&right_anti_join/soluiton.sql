dd0780f370655f0963f7c7ccbf0c629f90278500
Manish Madeshia <manishmadeshia1@gmail.com>
2026-08-29 10:53:10 +0530

    right anti join

--- day10_left_anti&right_anti_join/soluiton.sql
+++ day10_left_anti&right_anti_join/soluiton.sql
@@ -1,3 +1,5 @@
+----------------left join-----------------------
+
 select * from departments d left join employees e on d.departmentid = e.departmentid where e.departmentid is null
 select * from customers c left join orders o on c.customerid = o.customerid where o.customerid is null 
 select * from products p left join orderdetails os on p.productid = os.productid where os.productid is null
@@ -10,3 +12,11 @@ select c.customerid,c.customername,c.city from customers c left join orders o on
 
 select p.productid,p.productname,p.category,p.price from products p left join orderdetails os on p.productid = os.productid where os.productid is null
 select e.employeeid,e.firstname,e.salary from employees e left join orders o on e.employeeid = o.employeeid where o.orderid is null
+
+
+-----------------------right join--------------------
+
+select * from employees e right join departments d on e.departmentid = d.departmentid where e.employeeid is null
+select * from orders o right join customers c on o.customerid = c.customerid where o.orderid is null
+select * from orderdetails os right join products p on os.productid = p.productid where os.productid is null
+select * from orders o right join employees e on o.employeeid = e.employeeid where o.orderid is null
